b9c9fa37a9442a11df5394b482202d5dc8349e2d
Tyler Yang <tyler.yang@berkeley.edu>
2023-04-23 12:40:55 -0700

    [feat] start work on integrating better collisions

--- 3D Doodle Scratch/Assets/Scripts/AdversaryScript.cs
+++ 3D Doodle Scratch/Assets/Scripts/AdversaryScript.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class AdversaryScript : MonoBehaviour
 {
     // Start is called before the first frame update
-     public float speed = 5f; // The speed at which the enemy moves.
+    public float speed = 5f; // The speed at which the enemy moves.
     public float chaseDistance = 10f; // The maximum distance at which the enemy can chase the player.
     public Transform player; // A reference to the player object.
-    public bool playerSeen = false;
+
+    public float bounceForce = 30f;
+    private bool playerSeen = false;
 
     private Rigidbody rb; // The enemy's Rigidbody component.
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.isKinematic = true;
     }
 
     // Update is called once per frame
@@ -30,6 +31,22 @@ public class AdversaryScript : MonoBehaviour
         {
             playerSeen = true;
             rb.MovePosition(transform.position + direction * speed * Time.fixedDeltaTime);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Get the normal of the collision to determine if the player collided with the enemy from above
+            Vector3 contactNormal = collision.contacts[0].normal.normalized;
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            playerRigidbody.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+            Debug.Log(contactNormal.ToString());
+            if (contactNormal.y < -0.75) //adding some room for error
+            {
+                // If the player collided with the enemy from above, destroy the enemy
+                Destroy(gameObject);
+            }
         }
     }
 }
