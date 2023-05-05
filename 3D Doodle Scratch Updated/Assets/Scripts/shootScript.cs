ab3f3403d3fb540c6b78a3865bf2f786f5c263cb
Tyler Yang <tyler.yang@berkeley.edu>
2023-05-04 17:06:06 -0700

    [docs] update the final webpage, include iframes for videos with dimensions set to reduce content blur

--- 3D Doodle Scratch Updated/Assets/Scripts/shootScript.cs
+++ 3D Doodle Scratch Updated/Assets/Scripts/shootScript.cs
@@ -8,8 +8,10 @@ public class shootScript : MonoBehaviour
     public Transform pelletSpawn;
     public float fireRate = 0.1f;
 
+    public float pelletSpeed = 10f;
     private float nextFireTime = 0f;
 
+    private float maxDistance = 100f;
     void Update()
     {
         if (Input.GetButton("Fire1") && Time.time > nextFireTime)
@@ -21,6 +23,15 @@ public class shootScript : MonoBehaviour
 
     void FirePellet()
     {
-        Instantiate(pelletPrefab, pelletSpawn.position, pelletSpawn.rotation);
+        GameObject pellet = Instantiate(pelletPrefab, pelletSpawn.position, pelletSpawn.rotation);
+
+        // Rigidbody rb = pellet.GetComponent<Rigidbody>();
+        // rb.velocity = transform.forward * pelletSpeed;
+
+        // RaycastHit hit;
+        // if (Physics.Raycast(pelletSpawn, pelletSpawn.forward, out hit, maxDistance, enemyLayer))
+        // {
+        //     Destroy(hit.collider.gameObject);
+        // }
     }
 }
